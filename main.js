4aaf0812bc59551e1edb75002b1c496b613d7e78
SonyaRodina <sonya-rodina1@mail.ru>
2023-10-15 18:59:04 +0400

    'menu-js'

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 function myFunction() {
-    document.getElementById ("myDropdown").classList.toggle("show");
-  }
+  document.getElementById ("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.querySelector('.dropdown-content');
   
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      let dropdowns = document.getElementsByClassName('dropdown-content');
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+      if (dropdowns.classList.contains('show')) {
+        dropdowns.classList.remove('show');
       }
     }
   }
