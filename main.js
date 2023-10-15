7c7b2e5c7e40e2b770a74c7eabc36500a3de0366
SonyaRodina <sonya-rodina1@mail.ru>
2023-10-15 19:05:37 +0400

    'variant-menu-js'

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
+// function myFunction() {
+//   document.getElementById ("myDropdown").classList.toggle("show");
+// }
+
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     let dropdowns = document.querySelector('.dropdown-content');
+  
+//       if (dropdowns.classList.contains('show')) {
+//         dropdowns.classList.remove('show');
+//       }
+//     }
+//   }
+
 function myFunction() {
-  document.getElementById ("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    let dropdowns = document.querySelector('.dropdown-content');
-  
-      if (dropdowns.classList.contains('show')) {
-        dropdowns.classList.remove('show');
-      }
+if (!event.target.matches('.dropbtn')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
     }
   }
+}
+}
